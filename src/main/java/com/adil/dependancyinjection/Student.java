19cdc69f21c8f076644bd88dcf97cbc3a11a1b5b
Adil Naimi <adilnm23@gmail.com>
2020-11-29 02:29:11 -0600

    Inject setters

--- src/main/java/com/adil/dependancyinjection/Student.java
+++ src/main/java/com/adil/dependancyinjection/Student.java
@@ -2,4 +2,19 @@ package com.adil.dependancyinjection;
 
 public class Student {
 
+	private int id;
+	private String studentName;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public void display() {
+		System.out.println("This is the student: " + studentName + " and his id is " + id);
+	}
+
 }
